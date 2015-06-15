5f195be3dbe95ae12b25253a16ed63680f5ec339
vasilecristian <vasile.cristian@gmail.com>
2015-06-15 16:21:55 +0300

    - regenerate all projects.

--- prj/gen_prj.bat
+++ prj/gen_prj.bat
@@ -1,13 +1,12 @@
 @echo off 
 SET MAKETOOL=..\..\premake\release\premake5.exe
 
-rem %MAKETOOL% --to=vs2012 vs2012
-%MAKETOOL% --to=vs2013 vs2013
 
-rem %MAKETOOL% --arch=win8app --to=vs2013_w8 vs2013
-rem %MAKETOOL% --arch=macosx --to=xcode4\macosx xcode4
-%MAKETOOL% --arch=ios --to=xcode4\ios xcode4
 
-%MAKETOOL% --to=android_s2g android_s2g
-
-%MAKETOOL% --arch=win8app --to=vs2013_w8 vs2013
+%MAKETOOL% vs2013
+%MAKETOOL% --arch=ios xcode4
+%MAKETOOL% android_s2g
+%MAKETOOL% --arch=wp80app vs2012
+%MAKETOOL% --arch=w81app vs2013
+%MAKETOOL% --arch=wp81app vs2013
+%MAKETOOL% --arch=x32 --to=qmake qmake
